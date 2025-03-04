bfde43d4887b4e9ff701ed6c90f211a496edf1d9
A S H I K <ashik.moinul150@gmail.com>
2025-03-04 21:08:57 +0600

    Home/SearchBox

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -8,31 +8,70 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: tdBGColor,
-      appBar: AppBar(
-        backgroundColor: tdBGColor,
-        foregroundColor: Colors.black,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      appBar: _buildAppBar(),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
           children: [
-            Icon(
-              Icons.menu,
-              color: tdBlack,
-              size: 30,
-            ),
-            Container(
-              height: 40,
-              width: 40,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                image: DecorationImage(
-                    image: AssetImage('assets/images/260.jpg'),
-                    fit: BoxFit.cover),
-              ),
-            ),
+            searchBox(),
           ],
         ),
-       // centerTitle: true,
       ),
     );
   }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: tdBGColor,
+      foregroundColor: Colors.black,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Icon(
+            Icons.menu,
+            color: tdBlack,
+            size: 30,
+          ),
+          Container(
+            height: 40,
+            width: 40,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              image: const DecorationImage(
+                  image: AssetImage('assets/images/260.jpg'),
+                  fit: BoxFit.cover),
+            ),
+          ),
+        ],
+      ),
+      // centerTitle: true,
+    );
+  }
 }
+
+  Widget searchBox() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(0),
+          prefixIcon: Icon(
+            Icons.search,
+            color: tdBlack,
+            size: 20,
+          ),
+          prefixIconConstraints: BoxConstraints(
+            maxHeight: 20,
+            maxWidth: 20,
+          ),
+          border: InputBorder.none,
+          hintText: 'Search',
+          hintStyle: TextStyle(color: tdGrey),
+        ),
+      ),
+    );
+  }
