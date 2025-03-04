9d6b5821dc4dc17ec06e51c805c9c0e946611f54
A S H I K <ashik.moinul150@gmail.com>
2025-03-04 12:34:42 +0600

    Home/Creating Custom AppBar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/components/colors.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: tdBGColor,
+      appBar: AppBar(
+        backgroundColor: tdBGColor,
+        foregroundColor: Colors.black,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Icon(
+              Icons.menu,
+              color: tdBlack,
+              size: 30,
+            ),
+            Container(
+              height: 40,
+              width: 40,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                image: DecorationImage(
+                    image: AssetImage('assets/images/260.jpg'),
+                    fit: BoxFit.cover),
+              ),
+            ),
+          ],
+        ),
+       // centerTitle: true,
+      ),
+    );
   }
 }
