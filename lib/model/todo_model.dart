90f2a816938358dc2f556829dccb0a623eef373d
A S H I K <ashik.moinul150@gmail.com>
2025-03-05 23:02:56 +0600

    Add Todo Model

--- lib/model/todo_model.dart
+++ lib/model/todo_model.dart
@@ -10,8 +10,16 @@ class ToDo {
   });
   static List<ToDo> todoList() {
     return [
-      ToDo(id: '01', todoText: 'Morning Exercise', isDone: true),
-      ToDo(id: '02', todoText: 'Buy Groceries', isDone: true),
+      ToDo(
+        id: '01',
+        todoText: 'Morning Exercise',
+        isDone: true,
+      ),
+      ToDo(
+        id: '02',
+        todoText: 'Buy Groceries',
+        isDone: true,
+      ),
       ToDo(
         id: '03',
         todoText: 'Check Emails',
